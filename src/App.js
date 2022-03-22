020eb72938df142fe8bab8aa283cb5563f4bf981
PawelS1988 <szatkowskipawel1988@gmail.com>
2022-03-22 14:00:28 +0100

    display component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import { useSelector } from "react-redux";
-import { Title } from "./styled/Title";
-import { Section } from "./styled/Section";
-import Search from "./Componets/Search/Search";
-import { useEffect } from "react";
+import { useSelector } from 'react-redux'
+import { Title } from './styled/Title'
+import { Section } from './styled/Section'
+import Form from './Componets/Form/Form'
+import Search from './Componets/Search/Search'
+import Display from './Componets/Display/Display'
+import { useEffect } from 'react'
 
 function App() {
-  const contacts = useSelector((state) => state.contacts.items);
+  const contacts = useSelector((state) => state.contacts.items)
 
   useEffect(() => {
-    localStorage.setItem("contacts", JSON.stringify(contacts));
-  }, [contacts]);
+    localStorage.setItem('contacts', JSON.stringify(contacts))
+  }, [contacts])
 
-  console.log("contacts", contacts);
+  console.log('contacts', contacts)
 
   return (
     <Section>
       <Title>Phonebook</Title>
       <Form contacts={contacts} />
 
-      <Title as="h2">Contacts</Title>
+      <Title as='h2'>Contacts</Title>
       <Search />
       <Display contacts={contacts} />
     </Section>
-  );
+  )
 }
 
-export default App;
+export default App
